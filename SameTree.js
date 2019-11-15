0963304c22b44e8d976f3336a27a5ce6c0809d53
Melissa Stock <melissastock44@gmail.com>
2019-11-15 10:40:30 -0800

    solved leetcode 572

--- SameTree.js
+++ SameTree.js
@@ -33,7 +33,7 @@ Output: false
 
 */
 
-var isSameTree = function(p, q) {
+export var isSameTree = function(p, q) {
   if(p === null && q === null) return true;
   if(p === null || q === null) return false;
   if(p.val !== q.val) return false;
