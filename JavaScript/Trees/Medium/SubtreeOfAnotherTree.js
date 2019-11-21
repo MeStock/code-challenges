261dd85b4b1e286a7569c4e3d284bf9bff3cd026
Melissa Stock <melissastock44@gmail.com>
2019-11-21 13:43:57 -0800

    hackerrank class inheritance c++

--- JavaScript/Trees/Medium/SubtreeOfAnotherTree.js
+++ JavaScript/Trees/Medium/SubtreeOfAnotherTree.js
@@ -1,5 +1,5 @@
 
-import { isSameTree } from "./SameTree.js"; 
+import { isSameTree } from "../Easy/SameTree.js"; 
 
 /*
 Given two non-empty binary trees s and t, check whether tree t has exactly the same structure and node values with a subtree of s. A subtree of s is a tree consists of a node in s and all of this node's descendants. The tree s could also be considered as a subtree of itself.
