f28747d52d6194cd736cd8b508ee90533c7cd9b4
Melissa Stock <melissastock44@gmail.com>
2019-12-04 15:02:05 -0800

    hackerrank overload operator with Matricies

---
+++ C++/Medium/OverloadOperatorMatrix.cpp
@@ -0,0 +1,60 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+/*
+. You need to write the class Matrix which has a member a of type vector<vector<int> >. You also need to write a member function to overload the operator +. The function's job will be to add two objects of Matrix type and return the resultant Matrix.
+*/
+
+class Matrix{
+    public:
+        vector<vector<int>> a;
+        Matrix& operator+(const Matrix& y){
+            for(int i = 0; i < y.a.size(); ++i){
+                for(int j = 0; j < y.a.size(); ++j){
+                    this->a[i][j] += y.a[i][j];
+                }
+            }
+            return *this;
+        }
+};
+
+int main () {
+   int cases,k;
+   cin >> cases;
+   for(k=0;k<cases;k++) {
+      Matrix x;
+      Matrix y;
+      Matrix result;
+      int n,m,i,j;
+      cin >> n >> m;
+      for(i=0;i<n;i++) {
+         vector<int> b;
+         int num;
+         for(j=0;j<m;j++) {
+            cin >> num;
+            b.push_back(num);
+         }
+         x.a.push_back(b);
+      }
+      for(i=0;i<n;i++) {
+         vector<int> b;
+         int num;
+         for(j=0;j<m;j++) {
+            cin >> num;
+            b.push_back(num);
+         }
+         y.a.push_back(b);
+      }
+      result = x+y;
+      for(i=0;i<n;i++) {
+         for(j=0;j<m;j++) {
+            cout << result.a[i][j] << " ";
+         }
+         cout << endl;
+      }
+   }  
+   return 0;
+}
